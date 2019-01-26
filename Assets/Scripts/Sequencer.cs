98df01658130d29dd4932275872542e2c635549a
Andrew Lohmann <lohmann333@gmail.com>
2019-01-26 13:48:22 -0800

    Added the OnActivated function to the Sequencer.cs file

--- Assets/Scripts/Sequencer.cs
+++ Assets/Scripts/Sequencer.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class Sequencer : MonoBehaviour
 {
     public Activatable[] sequence;
+    /*
+     * object_0
+     * object_1
+     * object_2_0, object_2_1
+     * 
+     * check the tag up until the second underscore, then check after that and interpret it if there is anything
+     * */
+
 
     // Start is called before the first frame update
     void Start()
@@ -14,6 +22,19 @@ public class Sequencer : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    void AdvanceSequence()  //this changes the current state of the sequence to the next state, in order
+    {
+        if(false)//the current state is the last one, do not execute this function
+        {
+
+        }
+    }
+
+    public void OnActivated(Activatable newActivatable)
     {
         
     }
