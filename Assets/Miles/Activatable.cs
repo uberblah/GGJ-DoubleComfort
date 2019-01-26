c6e63290f8b5b47a61997fc0a1aa24ca06e8d685
Miles <miles.rufatlatre@colorado.edu>
2019-01-26 13:17:31 -0800

    Sequencer and Activatable skeletons

---
+++ Assets/Miles/Activatable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activatable : MonoBehaviour
+{
+    public Sequencer sequencer = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
