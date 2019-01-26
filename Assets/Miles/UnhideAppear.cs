a1b195ba2db6d08c2aab58549c0d12d830a5f95a
Miles <miles.rufatlatre@colorado.edu>
2019-01-26 13:01:32 -0800

    Missed some files apparently

---
+++ Assets/Miles/UnhideAppear.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnhideAppear : MonoBehaviour
+{
+    private System.Guid id;
+    private new Renderer renderer;
+    public FadeInOut fader = null;
+    
+    public SneakyItem item;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        id = System.Guid.NewGuid();
+        item.Register(id.ToString(), BeginFade);
+        if(fader == null)
+        {
+            fader = GetComponent<FadeInOut>();
+        }
+        if(fader == null)
+        {
+            Debug.LogError("Couldn't find a FadeInOut component on this object");
+            return;
+        }
+    }
+
+    public void BeginFade()
+    {
+        fader.FadeIn();
+    }
+}
