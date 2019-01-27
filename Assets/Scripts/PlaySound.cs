71e1f39a8184a43a181f4b987d6a98c904ba8527
Eduardo <rojase21@gmail.com>
2019-01-27 00:19:29 -0800

    play/stopsound

---
+++ Assets/Scripts/PlaySound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : Action
+{
+    private new AudioSource audio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitAction();
+        audio = GetComponent<AudioSource>();
+        if (audio == null)
+        {
+            Debug.LogError("Could not find an attached audio for the Audioplay component");
+            return;
+        }
+        audio.Stop();
+    }
+
+    // Update is called once per frame
+    public override void Do()
+    {
+        audio.Play();
+    }
+}
