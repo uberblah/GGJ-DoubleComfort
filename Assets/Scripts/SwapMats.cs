9e6bf5241280b2fb4c474d43068d1eb70df0d06b
Miles <miles.rufatlatre@colorado.edu>
2019-01-26 21:22:23 -0800

    Script to swap colors on objects!

---
+++ Assets/Scripts/SwapMats.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapMats : Action
+{
+    public Material[] mats1;
+    public Material[] mats2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitAction();
+    }
+
+    public override void Do()
+    {
+        Renderer renderer = gameObject.GetComponent<Renderer>();
+        Material[] imats1 = renderer.materials;
+        if(imats1[0] == mats1[0])
+        {
+            renderer.materials = mats2;
+        }
+        else
+        {
+            renderer.materials = mats1;
+        }
+    }
+}
