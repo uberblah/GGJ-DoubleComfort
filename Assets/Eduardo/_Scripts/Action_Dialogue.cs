99cdf5356b8cda7ff95cc6b1a2771dde30887f73
Eduardo <rojase21@gmail.com>
2019-01-26 18:53:40 -0800

    Dialogue Audio Attached

---
+++ Assets/Eduardo/_Scripts/Action_Dialogue.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Action_Dialogue : Action
+{
+
+
+    //public string
+    //public string blurb;
+
+    public DialogueTrigger trigger;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //overide Do function of Action button to do custome action
+    public void Do()
+    {
+        Debug.Log("You should override Action.Do in your subclass!");
+
+        //
+        trigger.TriggerDialogue();
+
+    }
+}
--- Assets/Eduardo/_Scripts/DialogueTrigger.cs
+++ Assets/Eduardo/_Scripts/DialogueTrigger.cs
@@ -7,13 +7,15 @@ public class DialogueTrigger : MonoBehaviour
     // initialize instance of dialogue bod
     public Dialogue dialogue;
 
+    public AudioSource audioClip;
+
     public void TriggerDialogue()
     {
 
         //tell the dialogue manager what dialogue to start
         // passes dialogue gameobject as a parameter
         //FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
-        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue, audioClip);
     }
 
 
