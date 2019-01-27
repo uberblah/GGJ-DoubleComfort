d6c9c9c23f73c786a1bba6364d23bc7254ab41bc
Karen Latre <kahatle123@gmail.com>
2019-01-27 12:29:31 -0800

    Character animation checkpoint commit

---
+++ Assets/Scripts/CharacterAnimatizer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimatizer : MonoBehaviour
+{
+    public Animator character;
+    public Animator glasses;
+    public string paramName = "Walking";
+
+    private void Start()
+    {
+        if(character == null)
+        {
+            Debug.LogError("Need a character GameObject for this CharacterAnimatizer");
+        }
+        if(glasses == null)
+        {
+            Debug.LogError("Need a glasses GameObject for this CharacterAnimatizer");
+        }
+    }
+
+    public void DoWalking()
+    {
+        character.SetBool(paramName, true);
+        glasses.SetBool(paramName, true);
+    }
+
+    public void DoIdle()
+    {
+        character.SetBool(paramName, false);
+        glasses.SetBool(paramName, false);
+    }
+}
