ffb8f0434bc90dd8b9e60d44113445c99fa51a2b
Miles <miles.rufatlatre@gmail.com>
2019-01-26 19:21:35 -0800

    merge commit

--- Assets/Eduardo/_Scripts/Action_Dialogue.cs
+++ Assets/Eduardo/_Scripts/Action_Dialogue.cs
@@ -1,42 +1,4 @@
-<<<<<<< HEAD
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Action_Dialogue : Action
-{
-
-
-    //public string
-    //public string blurb;
-
-    public DialogueTrigger trigger;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    //overide Do function of Action button to do custome action
-    public void Do()
-    {
-        Debug.Log("You should override Action.Do in your subclass!");
-
-        //
-        trigger.TriggerDialogue();
-
-    }
-}
-=======
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -73,3 +35,16 @@ public class Action_Dialogue : Action
     }
 }
 >>>>>>> merge commit
+=======
+    }
+
+    //overide Do function of Action button to do custome action
+    public void Do()
+    {
+        Debug.Log("You should override Action.Do in your subclass!");
+
+        //
+        trigger.TriggerDialogue();
+
+    }
+}
