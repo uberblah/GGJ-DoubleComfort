287ef970802855e900cf79c1aca14eed0325b95c
Miles <miles.rufatlatre@colorado.edu>
2019-01-27 16:43:17 -0800

    this is the uploaded state as of now

---
+++ Assets/Exiter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exiter : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("LASJGHKAJSHFKJASKJFHKASF");
+            Application.Quit();
+        }
+    }
+}
